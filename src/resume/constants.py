f237f04939f16614f063e3d115a39b408a0ce6f0
David Liu <david@davidliu.dev>
2025-09-08 04:23:15 -0400

    ResumeBuilder: Added settings for margins, font size, and part order in config.json

--- src/resume/constants.py
+++ src/resume/constants.py
@@ -2,6 +2,3 @@ NAME_SIZE = "size: 25pt"
 CONTACT = "size: 10pt"
 HEADER = "size: 15pt"
 INLINE_HEADER = "size: 13pt"
-MARGINS = "#set page(margin: 1cm)"
-TEXT = """#set text(font: "Arial", size: 11pt)"""
-
--- src/resume/resume_builder.py
+++ src/resume/resume_builder.py
@@ -8,15 +8,36 @@ from resume.projects import Projects
 
 
 class ResumeBuilder:
+    """
+    Generates the typst code required to build the resume
+    """
+
     def __init__(self, config_path: str = "config.json"):
         with open(config_path, "r") as file:
             self.config = json.load(file)
+            self.__parse_settings()
             self.__build_parts()
 
         file.close()
 
+    def __parse_settings(self):
+        self.part_order = ["header", 
+                           "education", 
+                           "skills", 
+                           "experience", 
+                           "projects"]
+        self.font_size = 11
+        self.margins = 1 # cm
+
+        if "settings" in self.config:
+            if "margins" in self.config["settings"]:
+                self.margins = self.config["settings"]["margins"]
+            if "font_size" in self.config["settings"]:
+                self.font_size = int(self.config["settings"]["font_size"])
+            if "part_order" in self.config["settings"]:
+                self.part_order = self.config["settings"]["part_order"]
+
     def __build_parts(self):
-        part_order = ["header", "education", "skills", "experience", "projects"]
         self.resume_parts = []
         self.resume_part_map = {
             "header": Header,
@@ -26,7 +47,7 @@ class ResumeBuilder:
             "projects": Projects
         }
 
-        for part_name in part_order:
+        for part_name in self.part_order:
             if (part_name in self.config 
                 and part_name in self.resume_part_map):
                 resp = self.config[part_name]
@@ -35,8 +56,8 @@ class ResumeBuilder:
 
     def build_resume(self):
         typst_str = f"""
-        {const.MARGINS}
-        {const.TEXT}
+        #set page(margin: {self.margins}cm)
+        #set text(font: "Arial", size: {self.font_size}pt)
         """
         for resume_part in self.resume_parts:
             typst_str += resume_part.to_typst_str()
